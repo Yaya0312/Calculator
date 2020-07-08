bf6fb6155aebfccb90dae4e99b2515d532515538
Yannis Yahi <yahiyanis@gmail.com>
2020-07-08 14:51:31 +0200

    :sparkles: completed

--- script.js
+++ script.js
@@ -1,16 +1,11 @@
 'use strict'
-
-const NUMBERS = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
-const OPERATORS = ['+','-','*','/','%'];
-const OTHERS = ['.']
-const ACCEPT_KEYS = ['0','1','2','3','4','5','6','7','8','9','+','-','*','/','%','.'] ;
+const OPERATOR = ['+','-','*','/','%'];
+const ACCEPT_KEYS = ['0','1','2','3','4','5','6','7','8','9','+','-','*','/','%','.'];
 const result = document.getElementById("res");
 
 function res() {
-  
   result.value = eval(result.value);
 }
-
 
 function clean() {
   result.value = "0";
@@ -18,7 +13,7 @@ function clean() {
 
 function put(n) {
   result.value = (result.value === "0") ? "" : result.value;
-
+  
   result.value = result.value + n;
 }
 
@@ -31,17 +26,23 @@ function back() {
 }
 
 function neg() {
-  res();
-  put('*(-1)');
-  res();
+  result.value = eval(result.value + "* (-1)");
 }
 
-document.addEventListener('keypress', (e) => {
+document.onkeydown = function (e) {
   if (ACCEPT_KEYS.includes(e.key)) {
     put(e.key);
-    
   }
   if (e.key === "Enter") {
     res();
   }
-});
+  if (e.key === "Backspace") {
+    back();
+  }
+  if (e.key === "Delete") {
+    clean();
+  }
+  if (e.key === "Shift") {
+    neg();
+  }
+}
